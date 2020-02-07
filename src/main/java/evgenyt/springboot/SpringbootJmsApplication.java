689102aabd2039a382f7860711d666c183bccfd7
EVGENY TYURIN <33890192+EvgenyTyurin@users.noreply.github.com>
2020-02-07 13:46:39 +0300

    ui improved

--- src/main/java/evgenyt/springboot/SpringbootJmsApplication.java
+++ src/main/java/evgenyt/springboot/SpringbootJmsApplication.java
@@ -1,6 +1,8 @@
 package evgenyt.springboot;
 
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -23,9 +25,18 @@ public class SpringbootJmsApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = 
 				SpringApplication.run(SpringbootJmsApplication.class, args);
-		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);		
-		System.out.println("Sending txt message...");
-		jmsTemplate.send("testQueue", session -> session.createTextMessage("Test text"));		
+		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+		System.out.println("Input message to send or exit to stop app:");
+		Scanner in = new Scanner(System.in);		
+		do {
+			// ask user for message
+			String s = in.next();
+			if (s.equals("exit"))
+				System.exit(0);
+			System.out.println("Sending txt message: " + s);
+			// send text to queue
+			jmsTemplate.send("testQueue", session -> session.createTextMessage(s));
+		} while (true);
 	}		
 		
 }
